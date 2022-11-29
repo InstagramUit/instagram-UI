3cc8b4f65eab4c75bbf9efe2089dec2757dcb6c9
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-11-29 20:51:46 +0700

    fix bug not show image

--- src/screens/NewPost/index.js
+++ src/screens/NewPost/index.js
@@ -53,10 +53,10 @@ const NewPost = () => {
             color: '#0D8BE7',
           }}>Next</Text>
       </View>
-      <Image
+      {/* <Image
         style={styles.stretch}
         source={require("../../storage/images/cover.png")}>
-      </Image>
+      </Image> */}
       <View 
       style={{
         justifyContent:'center',
