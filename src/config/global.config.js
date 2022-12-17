6d7fe2786b77d853ba0cbd257fb171098a299635
Kim Liên <62094889+Lien-1@users.noreply.github.com>
2022-12-17 15:45:07 +0700

    fix signin

--- src/config/global.config.js
+++ src/config/global.config.js
@@ -1,4 +1,4 @@
 module.exports = global.config = {
-  // API: "http://localhost:4000",
-  API: "https://2332-2001-ee0-50cd-5f30-8463-89b2-c2f-bf0.ap.ngrok.io",
+  API: "http://localhost:4000",
+  // API: "https://2332-2001-ee0-50cd-5f30-8463-89b2-c2f-bf0.ap.ngrok.io",
 };
